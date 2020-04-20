e7384ec21097f6b0c9d4f71cedcd0ffa07b31c6f
Colin Rathbun <colrath@gmail.com>
2020-04-20 14:13:25 -0600

    simplified and commented

--- getFileList.m
+++ getFileList.m
@@ -1,5 +1,10 @@
 function pathlist = getFileList
+% getFileList Opens a dialog to select files of type .nd2. Can select
+% multiple files using shift or command.
 [names, path] = uigetfile('*.nd2', 'Select some nd2 files.', 'MultiSelect', 'on');
+if ischar(names)
+    names = {names};
+end
 pathlist = {};
 for n=1:length(names)
     pathlist{n} = strcat(path,names{n});
--- processND2.m
+++ processND2.m
@@ -1,20 +1,18 @@
-function processed = processND2(pathlist, LoG_thresholds, show_LoG, high_pass_sigmas, LoG_sigmas, structuring_element, signal_count_limit)
-% set some default values
+function processed = processND2(pathlist, high_pass_sigmas)
+% processND2  Load ND2 files into memory and apply background correction to each frame.
+%   R = processND2(pathlist, sigmas) will apply 2D gaussian blurring of width
+%   sigma to each ND2 file in pathlist. The result is stored in R as a cell
+%   array.
+%
+%   The video is read in as a 5D array: [x, y, z, c, t]
+%
+%   Requires HIP from Janielia AIC. This also uses bfopen from the
+%   bioformat reader.
+
+
 if size(high_pass_sigmas,1) == 0
     high_pass_sigmas = [10 10 0];
 end
-if size(LoG_sigmas,1) == 0
-    LoG_sigmas = [2 2 0];
-end
-if size(structuring_element,1) == 0
-    kernel = strel("disk",1,4);
-    structuring_element = kernel.Neighborhood;
-end
-if size(signal_count_limit,1) == 0
-    signal_count_limit = 5;
-end
-
-
 
 processed = cell(pathlist(:));
 for f=1:length(pathlist)
@@ -22,38 +20,9 @@ for f=1:length(pathlist)
     raw = importND2(pathlist{f});
     
     % background subtract
-    processed{f,2} = HIP.HighPassFilter(raw, high_pass_sigmas,[]); % This
-    %is slower by a few seconds, but the below function might run out of
-    %memory with larger videos. Also the output of below is untested.
-    %processed{f,2} = highPassGaussian(raw, 10);
+    processed{f,2} = HIP.HighPassFilter(raw, high_pass_sigmas,[]); 
     
     clear('raw');
-    
-    % Log and find mask
-    log = HIP.LoG(processed{f,2}, LoG_sigmas,[]);
-    
-    % mask each channel individually
-    mask = NaN(size(log));
-    for c = 1:size(log,4)
-        mask(:,:,:,c,:) = log(:,:,:,c,:) < LoG_thresholds(c);
-    end
-    if show_LoG
-        openInIJ(log,1:50);
-        openInIJ(mask,1:50);
-    end
-    clear('log');
-    
-    % morphological opening on mask
-    kernel = strel("disk",1,4);
-    opened = HIP.Opener(mask, structuring_element,1,[]);
-    clear('mask');
-    
-    % mip that filters for pixels with 5 or more frames
-    s = cast(sum(opened,5),'single');
-    tokeep = s > signal_count_limit;
-    %processed{f,3} = cast(max(opened .* tokeep,[],5), 'single');
-    processed{f,3} = tokeep;
-    clear('opened');
     
     disp(['Done with ', pathlist{f}]);
 end
